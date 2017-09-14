623188d3ebd890abe8dfa196c0e65d8e1bba7819
kikoro86us <you@example.com>
2017-09-14 11:41:27 -0600

    titlechaged

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-? 'http://www.matthewhkim.com'
+? 'http://matthewhkim.com'
 : 'http://localhost:8080';
 
 export default PAYMENT_SERVER_URL;
